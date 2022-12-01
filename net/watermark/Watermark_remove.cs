c6a68c1e56831e31279d2c110212d9a27e613659
Evgeny.Pirogov <Evgeny.Pirogov@aspose.com>
2022-12-01 19:07:44 +0200

    get, remove

--- net/watermark/Watermark_remove.cs
+++ net/watermark/Watermark_remove.cs
@@ -1,10 +1,23 @@
+using Aspose.Pdf;
+using Aspose.Pdf.Annotations;
+
 namespace aspose_snippets.net
 {
     public static partial class Watermark
     {
         public static void remove()
         {
-            //call method
+            var pathSource = @"..\..\test.pdf";
+            Document document = new Document(pathSource);
+            var WatermarkAnnotations = document.Pages[1].Annotations
+                .Where(a => a.AnnotationType == AnnotationType.Watermark)
+                .Cast<WatermarkAnnotation>();
+            
+            Console.WriteLine("Watermarkes:");
+            foreach (var ca in WatermarkAnnotations)
+            {
+                document.Pages[1].Annotations.Remove(ca);
+            }
         }
     }
 }
