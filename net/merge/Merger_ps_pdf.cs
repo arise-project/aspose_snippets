e3ebe08912a76c59e234d0065300935f40070c88
Evgeny.Pirogov <Evgeny.Pirogov@aspose.com>
2022-12-08 08:42:28 +0200

    ps

--- net/merge/Merger_ps_pdf.cs
+++ net/merge/Merger_ps_pdf.cs
@@ -1,10 +1,31 @@
+using Aspose.Pdf;
+
 namespace aspose_snippets.net
 {
     public static partial class Merger
     {
         public static void ps_to_pdf()
         {
-            //call method
+            var pathSource1 = @"..\..\TestData\test.ps";
+            var pathSource2 = @"..\..\TestData\Second\test.ps";
+
+            var firstDoc = new Document(pathSource1, new PsLoadOptions());
+            var secondDoc = new Document(pathSource2,  new PsLoadOptions());
+
+            var outputDoc = new Document();
+            outputDoc.EnableObjectUnload = true;
+
+            foreach (var page in firstDoc.Pages)
+            {
+                outputDoc.Pages.Add(page);
+            }
+
+            foreach (var page in secondDoc.Pages)
+            {
+                outputDoc.Pages.Add(page);
+            }
+
+            outputDoc.Save("test.pdf", SaveFormat.Pdf);
         }
     }
 }
--- net/merge/Program.cs
+++ net/merge/Program.cs
@@ -29,8 +29,8 @@ namespace aspose_snippets.net.merger
             // Merger.pcl_to_pdf();
             // + Merger.tex_to_pdf();
             // Merger.svg_to_pdf();
-            Merger.mht_to_pdf();
-            // Merger.ps_to_pdf();
+            // + Merger.mht_to_pdf();
+            Merger.ps_to_pdf();
             // Merger.eps_to_pdf();
             // Merger.md_to_pdf();
             // + Merger.bmp_to_pdf();
