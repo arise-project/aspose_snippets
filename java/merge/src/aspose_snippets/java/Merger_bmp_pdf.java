e9970995461514c805af93245b42be1bfcc2654e
Evgeny.Pirogov <Evgeny.Pirogov@aspose.com>
2022-12-15 21:02:59 +0200

    pom.xml

--- java/merge/src/aspose_snippets/java/Merger_bmp_pdf.java
+++ java/merge/src/aspose_snippets/java/Merger_bmp_pdf.java
@@ -1,47 +1,47 @@
+
 package aspose_snippets.java;
 
 public class Merger_bmp_pdf {
     public static void Execute() {
-        var pathSource1 = @"..\..\TestData\test.bmp";
-            var pathSource2 = @"..\..\TestData\Second\test.bmp";
+        string pathSource1 = "../../../../TestData/test.bmp";
+        string pathSource2 = "../../../../TestData/Second/test.bmp";
 
-            //create empty pdf document
-            using var doc = new Aspose.Pdf.Document();
+        //create empty pdf document
+        var doc = new com.aspose.pdf.Document();
 
-            //make list of file streams with documents to merge
-            var streams = new List<Stream>() 
-            { 
-                File.Open(pathSource1, FileMode.Open), 
-                File.Open(pathSource2, FileMode.Open) 
-            };
+        //make list of file streams with documents to merge
+        var streams = new List<Stream>() {
+                File.Open(pathSource1,FileMode.Open),
+                    File.Open(pathSource2,FileMode.Open)
+        };
 
-            foreach (var fs in streams)
-            {
-                //add new page to pdf
-                Aspose.Pdf.Page page = doc.Pages.Add();
-                
-                //setup page size to be A4
-                page.SetPageSize(
-                    Aspose.Pdf.PageSize.A4.Width, 
+        foreach(var fs in streams)
+        {
+            //add new page to pdf
+            Aspose.Pdf.Page page = doc.Pages.Add();
+
+            //setup page size to be A4
+            page.SetPageSize(
+                    Aspose.Pdf.PageSize.A4.Width,
                     Aspose.Pdf.PageSize.A4.Height);
 
-                Aspose.Pdf.Rectangle rect;
-Ы
-                //load image from stream, it suport a lot of formats
-                using (Aspose.Imaging.Image image = Aspose.Imaging.Image.Load(fs))
-                {
-                    //read image dimensions to pdf page rectangle
-                    rect = new Aspose.Pdf.Rectangle(0, 0, image.Width - 1, image.Height - 1);
-                }
-
-                //reset stream to read from begin for next step
-                fs.Seek(0, SeekOrigin.Begin);
-
-                //add image to new pdf page
-                page.AddImage(fs, rect);
+            Aspose.Pdf.Rectangle rect;
+            Ы
+            //load image from stream, it suport a lot of formats
+            using(Aspose.Imaging.Image image = com.aspose.imaging.Imaging.Image.Load(fs))
+            {
+                //read image dimensions to pdf page rectangle
+                rect = new Aspose.Pdf.Rectangle(0, 0, image.Width - 1, image.Height - 1);
             }
 
-            //save result pdf to file
-            doc.Save("test.pdf", Aspose.Pdf.SaveFormat.Pdf);
+            //reset stream to read from begin for next step
+            fs.Seek(0, SeekOrigin.Begin);
+
+            //add image to new pdf page
+            page.AddImage(fs, rect);
+        }
+
+        //save result pdf to file
+        doc.Save("test.pdf", Aspose.Pdf.SaveFormat.Pdf);
     }
 }
