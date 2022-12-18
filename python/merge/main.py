df6dd8bada775cb00e0a0b0c55afd6180ecd62b2
Evgeny <Evgeny.Pirogov@aspose.com>
2022-12-18 17:37:31 +0200

    python example

---
+++ python/merge/main.py
@@ -0,0 +1,11 @@
+import example_get_started
+
+def main():
+
+    example = example_get_started.HelloWorld("<license_path>")
+    example.run_simple()
+    example.run_complex()
+
+
+if __name__ == '__main__':
+    main()
