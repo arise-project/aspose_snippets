d4e9964ca3903d845f1ddd6ab2480bc9ef3a357f
Evgeny Pirogov <Evgeny.Pirogov@aspose.com>
2022-12-19 05:31:03 +0200

    cpp stubs

--- python/merge/Merger_bmp_pdf.py
+++ python/merge/Merger_bmp_pdf.py
@@ -3,15 +3,11 @@ import clr
 aspose_pdf = clr.AddReference("../../lib/Aspose.PDF.dll")
 
 from System import TimeSpan
+from Aspose.Pdf import Document
 
-from Aspose.Pdf import Document, Color, License, BorderInfo, BorderSide, Rectangle, HorizontalAlignment
-from Aspose.Pdf import Table, MarginInfo
-from Aspose.Pdf.Text import TextFragment, Position, TextBuilder,FontRepository
-
-
-class Merger_bmp_pdf(object):
+class bmp_to_pdf(object):
     def __init__(self,licence_path):
-        self.dataDir = "C:\\Samples\\"
+        self.dataDir = "../../TestData"
         if licence_path:
             self.licence_path = licence_path
             self.aspose_license = License()
@@ -19,23 +15,4 @@ class Merger_bmp_pdf(object):
 
     def exec(self):
 
-        # Initialize document object
-        document = Document()
-        # Add page
-        page = document.Pages.Add()
-        # Add text to new page
-        textFragment = TextFragment("Hello,world!")
-        textFragment.Position = Position(100, 600)
-
-        textFragment.TextState.FontSize = 12
-        textFragment.TextState.Font = FontRepository.FindFont("TimesNewRoman")
-        textFragment.TextState.BackgroundColor = Color.Blue
-        textFragment.TextState.ForegroundColor = Color.Yellow
-
-        # Create TextBuilder object
-        textBuilder = TextBuilder(page)
-
-        # Append the text fragment to the PDF page
-        textBuilder.AppendText(textFragment)
-
-        document.Save("HelloWorld_out.pdf")
+        
