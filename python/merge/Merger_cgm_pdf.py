e7366fecc26a732af58810e6971930060a31808a
Evgeny.Pirogov <Evgeny.Pirogov@aspose.com>
2022-12-21 12:19:36 +0200

    cgm

--- python/merge/Merger_cgm_pdf.py
+++ python/merge/Merger_cgm_pdf.py
@@ -1,12 +1,14 @@
+from Aspose.Pdf import SaveFormat
+from Aspose.Pdf import CgmLoadOptions
+from Aspose.Pdf import Document
+from System import TimeSpan
 import clr
 
 aspose_pdf = clr.AddReference("../../lib/Aspose.PDF.dll")
 
-from System import TimeSpan
-from Aspose.Pdf import Document
 
 class cgm_to_pdf(object):
-    def __init__(self,licence_path):
+    def __init__(self, licence_path):
         self.dataDir = "../../TestData"
         if licence_path:
             self.licence_path = licence_path
@@ -15,4 +17,26 @@ class cgm_to_pdf(object):
 
     def exec(self):
 
-        
+        pathSource1 = "../../TestData/test.cgm"
+        pathSource2 = "../../TestData/Second/test.cgm"
+
+        # /cgm files can be parsed and loaded as Aspose Document
+        firstDoc = Document(pathSource1, CgmLoadOptions)
+        secondDoc = Document(pathSource2, CgmLoadOptions)
+
+        # create empty pdf document
+        outputDoc = Document
+
+        # set less memory usage with unload instead of fast performance
+        outputDoc.EnableObjectUnload = true
+
+        for page in firstDoc.Pages:
+            # add page from one document to another directly
+            outputDoc.Pages.Add(page)
+
+        for page in secondDoc.Pages:
+            # add page from one document to another directly
+            outputDoc.Pages.Add(page)
+
+        # save result pdf to file
+        outputDoc.Save("test.pdf", SaveFormat.Pdf)
