1081334fe604e5268cd08c6ad624280643565d6b
Evgeny.Pirogov <Evgeny.Pirogov@aspose.com>
2022-12-21 13:22:11 +0200

    md

--- python/merge/Merger_md_pdf.py
+++ python/merge/Merger_md_pdf.py
@@ -4,6 +4,8 @@ aspose_pdf = clr.AddReference("../../lib/Aspose.PDF.dll")
 
 from System import TimeSpan
 from Aspose.Pdf import Document
+from Aspose.Pdf import MdLoadOptions
+from Aspose.Pdf import SaveFormat
 
 class md_to_pdf(object):
     def __init__(self,licence_path):
@@ -15,4 +17,26 @@ class md_to_pdf(object):
 
     def exec(self):
 
-        
+        pathSource1 = "../../TestData/test.md"
+        pathSource2 = "../../TestData/Second/test.md"
+
+        #Markdown files can be parsed and loaded as Aspose Document
+        firstDoc = Document(pathSource1, MdLoadOptions)
+        secondDoc = Document(pathSource2, MdLoadOptions)
+
+        #create empty pdf document
+        outputDoc = Document();
+
+        #set less memory usage with unload instead of fast performance
+        outputDoc.EnableObjectUnload = true
+
+        for page in firstDoc.Pages:
+            #add page from one document to another directly
+            outputDoc.Pages.Add(page)
+
+        for page in secondDoc.Pages:
+            #add page from one document to another directly
+            outputDoc.Pages.Add(page)
+
+        #save result pdf to file
+        outputDoc.Save("test.pdf", SaveFormat.Pdf)
