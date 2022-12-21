cef20a35eb86787b448f18e63de38332bd9724d2
Evgeny.Pirogov <Evgeny.Pirogov@aspose.com>
2022-12-21 13:36:04 +0200

    pcl

--- python/merge/Merger_pcl_pdf.py
+++ python/merge/Merger_pcl_pdf.py
@@ -1,12 +1,14 @@
+from Aspose.Pdf import SaveFormat
+from Aspose.Pdf import PclLoadOptions
+from Aspose.Pdf import Document
+from System import TimeSpan
 import clr
 
 aspose_pdf = clr.AddReference("../../lib/Aspose.PDF.dll")
 
-from System import TimeSpan
-from Aspose.Pdf import Document
 
 class pcl_to_pdf(object):
-    def __init__(self,licence_path):
+    def __init__(self, licence_path):
         self.dataDir = "../../TestData"
         if licence_path:
             self.licence_path = licence_path
@@ -15,4 +17,34 @@ class pcl_to_pdf(object):
 
     def exec(self):
 
-        
+        pathSource1 = "../../TestData/test.pcl"
+        pathSource2 = "../../TestData/Second/test.pcl"
+
+        # suspend not critical errors
+        opt1 = PclLoadOptions
+        opt1.SupressErrors = true
+
+        # pcl files can be parsed and loaded as Aspose Document
+        firstDoc = Document(pathSource1, opt1)
+
+        # suspend not critical errors
+        opt2 = PclLoadOptions
+        opt2.SupressErrors = true
+        secondDoc = Document(pathSource2, opt2)
+
+        # create empty pdf document
+        outputDoc = Document
+
+        # set less memory usage with unload instead of fast performance
+        outputDoc.EnableObjectUnload = true
+
+        for page in firstDoc.Pages:
+            # add page from one document to another directly
+            outputDoc.Pages.Add(page)
+
+        for page in secondDoc.Pages:
+            # add page from one document to another directly
+            outputDoc.Pages.Add(page)
+
+        # save result pdf to file
+        outputDoc.Save("test.pdf", SaveFormat.Pdf)
