1abe78a87db6397d7b7eaf82d5ebda8987635ba6
Evgeny.Pirogov <Evgeny.Pirogov@aspose.com>
2022-12-21 12:58:58 +0200

    html

--- python/merge/Merger_html_pdf.py
+++ python/merge/Merger_html_pdf.py
@@ -1,12 +1,14 @@
+from Aspose.Pdf import SaveFormat
+from Aspose.Pdf import HtmlLoadOptions
+from Aspose.Pdf import Document
+from System import TimeSpan
 import clr
 
 aspose_pdf = clr.AddReference("../../lib/Aspose.PDF.dll")
 
-from System import TimeSpan
-from Aspose.Pdf import Document
 
 class html_to_pdf(object):
-    def __init__(self,licence_path):
+    def __init__(self, licence_path):
         self.dataDir = "../../TestData"
         if licence_path:
             self.licence_path = licence_path
@@ -15,4 +17,39 @@ class html_to_pdf(object):
 
     def exec(self):
 
-        
+        pathSource1 = "../../TestData/test.html"
+        pathSource2 = "../../TestData/Second/test.html"
+
+        opt1 = HtmlLoadOptions
+        # set html encodyng
+        opt1.InputEncoding = "UTF-8",
+        # render all html to single large pdf page
+        opt1.IsRenderToSinglePage = true
+
+        # html files can be parsed and loaded as Aspose Document
+        firstDoc = Document(pathSource1, opt1)
+
+        opt2 = HtmlLoadOptions
+        # set html encodyng
+        opt2.InputEncoding = "UTF-8",
+        # split html content to pdf pages
+        opt2.IsRenderToSinglePage = false
+
+        secondDoc = Document(pathSource2, opt2)
+
+        # create empty pdf document
+        outputDoc = Document()
+
+        # set less memory usage with unload instead of fast performance
+        outputDoc.EnableObjectUnload = true
+
+        for page in firstDoc.Pages:
+            # add page from one document to another directly
+            outputDoc.Pages.Add(page)
+
+        for page in secondDoc.Pages:
+            # add page from one document to another directly
+            outputDoc.Pages.Add(page)
+
+        # save result pdf to file
+        outputDoc.Save("test.pdf", SaveFormat.Pdf)
