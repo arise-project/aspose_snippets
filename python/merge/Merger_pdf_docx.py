20d32f8acf82fc242c8561446f640e19f00e1fb0
Evgeny.Pirogov <Evgeny.Pirogov@aspose.com>
2022-12-21 14:26:50 +0200

    docx

--- python/merge/Merger_pdf_docx.py
+++ python/merge/Merger_pdf_docx.py
@@ -1,12 +1,15 @@
+from Aspose.Pdf.DocSaveOptions import RecognitionMode
+from Aspose.Pdf.DocSaveOptions import DocFormat
+from Aspose.Pdf import DocSaveOptions
+from Aspose.Pdf import Document
+from System import TimeSpan
 import clr
 
 aspose_pdf = clr.AddReference("../../lib/Aspose.PDF.dll")
 
-from System import TimeSpan
-from Aspose.Pdf import Document
 
 class pdf_to_docx(object):
-    def __init__(self,licence_path):
+    def __init__(self, licence_path):
         self.dataDir = "../../TestData"
         if licence_path:
             self.licence_path = licence_path
@@ -15,4 +18,32 @@ class pdf_to_docx(object):
 
     def exec(self):
 
-        
+        pathSource1 = "../../TestData/test.pdf"
+        pathSource2 = "../../TestData/Second/test.pdf"
+
+        # read pdf file to Aspose Document
+        firstDoc = Document(pathSource1)
+        secondDoc = Document(pathSource2)
+
+        # create empty pdf document
+        outputDoc = Document
+
+        # set less memory usage with unload instead of fast performance
+        outputDoc.EnableObjectUnload = True
+
+        for page in firstDoc.Pages:
+            # add page from one document to another directly
+            outputDoc.Pages.Add(page)
+
+        for page in secondDoc.Pages:
+            # add page from one document to another directly
+            outputDoc.Pages.Add(page)
+
+        opt1 = DocSaveOptions
+        # use docx format
+        opt1.Format = DocFormat.DocX,
+        # make document editable flow and recognize of tables
+        opt1.Mode = RecognitionMode.EnhancedFlow
+
+        # save pdf to Microsoft Word docx format
+        outputDoc.Save("test.docx", opt1)
