476a6411c9d397ea9024799aeda866a39e0be446
Evgeny.Pirogov <Evgeny.Pirogov@aspose.com>
2022-12-21 14:46:52 +0200

    emf

--- python/merge/Merger_pdf_emf.py
+++ python/merge/Merger_pdf_emf.py
@@ -1,12 +1,14 @@
+from Aspose.Pdf.EpubSaveOptions import RecognitionMode
+from Aspose.Pdf import EpubSaveOptions
+from Aspose.Pdf import Document
+from System import TimeSpan
 import clr
 
 aspose_pdf = clr.AddReference("../../lib/Aspose.PDF.dll")
 
-from System import TimeSpan
-from Aspose.Pdf import Document
 
 class pdf_to_emf(object):
-    def __init__(self,licence_path):
+    def __init__(self, licence_path):
         self.dataDir = "../../TestData"
         if licence_path:
             self.licence_path = licence_path
@@ -15,4 +17,30 @@ class pdf_to_emf(object):
 
     def exec(self):
 
-        
+        pathSource1 = "../../TestData/test.pdf"
+        pathSource2 = "../../TestData/Second/test.pdf"
+
+        # read pdf file to Aspose Document
+        firstDoc = Document(pathSource1)
+        secondDoc = Document(pathSource2)
+
+        # create empty pdf document
+        outputDoc = Document()
+
+        # set less memory usage with unload instead of fast performance
+        outputDoc.EnableObjectUnload = True
+
+        for page in firstDoc.Pages:
+            # add page from one document to another directly
+            outputDoc.Pages.Add(page)
+
+        for page in secondDoc.Pages:
+            # add page from one document to another directly
+            outputDoc.Pages.Add(page)
+
+        opt1 = EpubSaveOptions
+        # keep the logical structure of transformed document
+        opt1.ContentRecognitionMode = RecognitionMode.PdfFlow
+
+        # save pdf to epub
+        outputDoc.Save("test.epub", opt1)
