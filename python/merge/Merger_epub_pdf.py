788173a8d2b511cd7f3f13fa3353daf176e39603
Evgeny.Pirogov <Evgeny.Pirogov@aspose.com>
2022-12-21 12:50:26 +0200

    ebub

--- python/merge/Merger_epub_pdf.py
+++ python/merge/Merger_epub_pdf.py
@@ -1,12 +1,16 @@
+from Aspose.Pdf import SaveFormat
+from Aspose.Pdf.LoadOptions import MarginsAreaUsageModes
+from Aspose.Pdf.LoadOptions import PageSizeAdjustmentModes
+from Aspose.Pdf import EpubLoadOptions
+from Aspose.Pdf import Document
+from System import TimeSpan
 import clr
 
 aspose_pdf = clr.AddReference("../../lib/Aspose.PDF.dll")
 
-from System import TimeSpan
-from Aspose.Pdf import Document
 
 class epub_to_pdf(object):
-    def __init__(self,licence_path):
+    def __init__(self, licence_path):
         self.dataDir = "../../TestData"
         if licence_path:
             self.licence_path = licence_path
@@ -15,4 +19,38 @@ class epub_to_pdf(object):
 
     def exec(self):
 
-        
+        pathSource1 = "../../TestData/test.epub"
+        pathSource2 = "../../TestData/Second/test.epub"
+
+        opt1 = EpubLoadOptions
+        # use algorithm to prevent content to be truncated
+        opt1.PageSizeAdjustmentMode = PageSizeAdjustmentModes.EnlargeRequiredViewportWidthAndDoConversionAgain
+        # usage of margins area during conversion
+        opt1.MarginsAreaUsageMode = MarginsAreaUsageModes.PutContentOnMarginAreaIfNecessary
+
+        # epub files can be parsed and loaded as Aspose Document
+        firstDoc = Document(pathSource1, opt1)
+
+        opt2 = EpubLoadOptions
+        # use algorithm to prevent content to be truncated
+        opt1.PageSizeAdjustmentMode = PageSizeAdjustmentModes.EnlargeRequiredViewportWidthAndDoConversionAgain
+        # usage of margins area during conversion
+        opt1.MarginsAreaUsageMode = MarginsAreaUsageModes.PutContentOnMarginAreaIfNecessary
+        secondDoc = Document(pathSource2, opt2)
+
+        # create empty pdf document
+        outputDoc = Document
+
+        # set less memory usage with unload instead of fast performance
+        outputDoc.EnableObjectUnload = true
+
+        for page in firstDoc.Pages:
+            # add page from one document to another directly
+            outputDoc.Pages.Add(page)
+
+        for page in secondDoc.Pages:
+            # add page from one document to another directly
+            outputDoc.Pages.Add(page)
+
+        # save result pdf to file
+        outputDoc.Save("test.pdf", SaveFormat.Pdf)
