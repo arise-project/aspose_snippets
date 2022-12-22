01adf152cb46503f394ce37aa331bf25d6e8d176
Evgeny.Pirogov <Evgeny.Pirogov@aspose.com>
2022-12-22 11:05:25 +0200

    license

---
+++ cpp/split/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "Aspose.PDF.Cpp/PdfLicense.h"
+
+#include "system/string.h"
+#include "system/io/file.h"
+#include "drawing/imaging/image_format.h"
+
+using namespace System;
+using namespace Aspose::Pdf;
+
+void bmp_to_pdf()
+{
+    auto lic = MakeObject<License>();
+    lic->SetLicense(u"../../test.lic");
+}
+
