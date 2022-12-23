cb0f67f3e234a577e65273e123e96305ad4c9570
Evgeny Pirogov <Evgeny.Pirogov@aspose.com>
2022-12-23 16:41:42 +0200

    claen

--- cpp/merge/Merger_epub_pdf.cpp
+++ cpp/merge/Merger_epub_pdf.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
 #include "Aspose.PDF.Cpp/Document.h"
-#include "Aspose.PDF.Cpp/PdfLicense.h"
 #include "Aspose.PDF.Cpp/IO/ConvertStrategies/PdfFormat.h"
 #include "Aspose.PDF.Cpp/PdfFormatConversionOptions.h"
 #include "Aspose.PDF.Cpp/Page.h"
@@ -9,20 +8,9 @@
 #include "Aspose.PDF.Cpp/EpubLoadOptions.h"
 #include "Aspose.PDF.Cpp/LoadOptions/PageSizeAdjustmentModes.h"
 #include "Aspose.PDF.Cpp/LoadOptions/MarginsAreaUsageModes.h"
-#include "Aspose.PDF.Cpp/Devices/BmpDevice.h"
-#include "Aspose.PDF.Cpp/Devices/EmfDevice.h"
-#include "Aspose.PDF.Cpp/Devices/JpegDevice.h"
-#include "Aspose.PDF.Cpp/Devices/PngDevice.h"
-#include "Aspose.PDF.Cpp/Devices/TextDevice.h"
-#include "Aspose.PDF.Cpp/Facades/PdfConverter.h"
-#include "Aspose.PDF.Cpp/Generator/Paragraphs.h"
-#include "Aspose.PDF.Cpp/Text/TextAbsorber.h"
-#include "Aspose.PDF.Cpp/Text/TextFragment.h"
 #include "Aspose.PDF.Cpp/SaveFormat.h"
 
 #include "system/string.h"
-#include "system/io/file.h"
-#include "drawing/imaging/image_format.h"
 
 using namespace System;
 using namespace Aspose::Pdf;
@@ -47,7 +35,7 @@ void epub_to_pdf()
     // usage of margins area during conversion
     opt2->MarginsAreaUsageMode = MarginsAreaUsageModes::PutContentOnMarginAreaIfNecessary;
 
-    com.aspose.pdf.Document secondDoc =MakeObject<Document>(pathSource2, opt2);
+    auto secondDoc =MakeObject<Document>(pathSource2, opt2);
 
     // create empty pdf document
     auto outputDoc = MakeObject<Document>();
--- cpp/merge/Merger_pdf_docx.cpp
+++ cpp/merge/Merger_pdf_docx.cpp
@@ -1,27 +1,15 @@
 #include <iostream>
 
 #include "Aspose.PDF.Cpp/Document.h"
-#include "Aspose.PDF.Cpp/PdfLicense.h"
 #include "Aspose.PDF.Cpp/IO/ConvertStrategies/PdfFormat.h"
 #include "Aspose.PDF.Cpp/PdfFormatConversionOptions.h"
 #include "Aspose.PDF.Cpp/Page.h"
 #include "Aspose.PDF.Cpp/PageCollection.h"
 #include "Aspose.PDF.Cpp/DocSaveOptions/DocFormat.h"
 #include "Aspose.PDF.Cpp/DocSaveOptions/RecognitionMode.h"
-#include "Aspose.PDF.Cpp/Devices/BmpDevice.h"
-#include "Aspose.PDF.Cpp/Devices/EmfDevice.h"
-#include "Aspose.PDF.Cpp/Devices/JpegDevice.h"
-#include "Aspose.PDF.Cpp/Devices/PngDevice.h"
-#include "Aspose.PDF.Cpp/Devices/TextDevice.h"
-#include "Aspose.PDF.Cpp/Facades/PdfConverter.h"
-#include "Aspose.PDF.Cpp/Generator/Paragraphs.h"
-#include "Aspose.PDF.Cpp/Text/TextAbsorber.h"
-#include "Aspose.PDF.Cpp/Text/TextFragment.h"
 #include "Aspose.PDF.Cpp/SaveFormat.h"
 
 #include "system/string.h"
-#include "system/io/file.h"
-#include "drawing/imaging/image_format.h"
 
 using namespace System;
 using namespace Aspose::Pdf;
