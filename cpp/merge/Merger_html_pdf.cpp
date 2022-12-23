d7e425295e3dd0505ddc993f14165ddf06627dfc
Evgeny Pirogov <Evgeny.Pirogov@aspose.com>
2022-12-23 12:41:34 +0200

    html

--- cpp/merge/Merger_html_pdf.cpp
+++ cpp/merge/Merger_html_pdf.cpp
@@ -5,6 +5,7 @@
 #include "Aspose.PDF.Cpp/IO/ConvertStrategies/PdfFormat.h"
 #include "Aspose.PDF.Cpp/PdfFormatConversionOptions.h"
 #include "Aspose.PDF.Cpp/Page.h"
+#include "Aspose.PDF.Cpp/HtmlLoadOptions.h"
 #include "Aspose.PDF.Cpp/PageCollection.h"
 #include "Aspose.PDF.Cpp/Devices/BmpDevice.h"
 #include "Aspose.PDF.Cpp/Devices/EmfDevice.h"
@@ -29,23 +30,23 @@ void html_to_pdf()
     auto pathSource1 = u"../../../../TestData/test.html";
     auto pathSource2 = u"../../../../TestData/Second/test.html";
 
-    com.aspose.pdf.HtmlLoadOptions opt1 = new com.aspose.pdf.HtmlLoadOptions();
+    auto opt1 = MakeObject<HtmlLoadOptions>();
 
     // set html encodyng
-    opt1.setInputEncoding("UTF-8");
+    opt1->InputEncoding = u"UTF-8";
     // render all html to single large pdf page
-    opt1.setRenderToSinglePage(true);
+    opt1->RenderToSinglePage = true;
 
     // html files can be parsed and loaded as Aspose Document
-    com.aspose.pdf.Document firstDoc = new com.aspose.pdf.Document(pathSource1, opt1);
+    auto firstDoc = MakeObject<Document>(pathSource1, opt1);
 
-    com.aspose.pdf.HtmlLoadOptions opt2 = new com.aspose.pdf.HtmlLoadOptions();
+    auto opt2 = MakeObject<HtmlLoadOptions>();
     // set html encodyng
-    opt2.setInputEncoding("UTF-8");
+    opt2->InputEncoding = u"UTF-8";
     // split html content to pdf pages
-    opt2.setRenderToSinglePage(false);
+    opt2->RenderToSinglePage = false;
 
-    com.aspose.pdf.Document secondDoc = new com.aspose.pdf.Document(pathSource2, opt2);
+    auto secondDoc = MakeObject<Document>(pathSource2, opt2);
 
     // create empty pdf document
     auto outputDoc = MakeObject<Document>();
