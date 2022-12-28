a82b6b3b2a9e3d004060c450a56c83933ac89144
Evgeny.Pirogov <Evgeny.Pirogov@aspose.com>
2022-12-28 08:20:54 +0200

    run cpp

--- cpp/merge/merge.cpp
+++ cpp/merge/merge.cpp
@@ -2,10 +2,55 @@
 //
 
 #include <iostream>
+#include "Merger_cgm_pdf.cpp"
+#include "Merger_eps_pdf.cpp"
+#include "Merger_epub_pdf.cpp"
+#include "Merger_html_pdf.cpp"
+#include "Merger_md_pdf.cpp"
+#include "Merger_mht_pdf.cpp"
+#include "Merger_pcl_pdf.cpp"
+#include "Merger_pdf_doc.cpp"
+#include "Merger_pdf_epub.cpp"
+#include "Merger_pdf_docx.cpp"
+#include "Merger_pdf_html.cpp"
+#include "Merger_pdf_pptx.cpp"
+#include "Merger_pdf_pdfa.cpp"
+#include "Merger_pdf_svg.cpp"
+#include "Merger_pdf_tex.cpp"
+#include "Merger_pdf_text.cpp"
+#include "Merger_pdf_xls.cpp"
+#include "Merger_pdf_xps.cpp"
+#include "Merger_ps_pdf.cpp"
+#include "Merger_svg_pdf.cpp"
+#include "Merger_tex_pdf.cpp"
+#include "Merger_xps_pdf.cpp"
 
 int main()
 {
     std::cout << "Hello World!\n";
+
+    cgm_to_pdf();
+    eps_to_pdf();
+    epub_to_pdf();
+    html_to_pdf();
+    md_to_pdf();
+    mht_to_pdf();
+    pcl_to_pdf();
+    pdf_to_doc();
+    pdf_to_docx();
+    pdf_to_epub();
+    pdf_to_html();
+    pdf_to_pdfa();
+    pdf_to_pptx();
+    pdf_to_svg();
+    pdf_to_tex();
+    pdf_to_text();
+    pdf_to_xls();
+    pdf_to_xps();
+    ps_to_pdf();
+    svg_to_pdf();
+    tex_to_pdf();
+    xps_to_pdf();
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
