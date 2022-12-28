956c511edc8e53ccc223a72abd5bbbdf910be306
Evgeny.Pirogov <Evgeny.Pirogov@aspose.com>
2022-12-28 16:27:30 +0200

    tiff

--- cpp/merge/_Merger_pdf_tiff.cpp
+++ cpp/merge/_Merger_pdf_tiff.cpp
@@ -80,44 +80,22 @@ void pdf_to_tiff()
 		GetEncoderClsid(L"image/tiff", &encoderClsid);
 
 		// Create four image objects.
-		Image* multi = new Image(L"Shapes.bmp");
-		Image* page2 = new Image(L"Cereal.gif");
-		Image* page3 = new Image(L"Iron.jpg");
-		Image* page4 = new Image(L"House.png");
+		Image* multi = new Image(L"1_test.jpg");
+		Image* page2 = new Image(L"2_test.jpg");
 
 		// Save the first page (frame).
 		parameterValue = EncoderValueMultiFrame;
-		stat = multi->Save(L"MultiFrame.tif", &encoderClsid, &encoderParameters);
-		if (stat == Ok)
-			printf("Page 1 saved successfully.\n");
-
+		stat = multi->Save(L"test.tif", &encoderClsid, &encoderParameters);
+		
 		// Save the second page (frame).
 		parameterValue = EncoderValueFrameDimensionPage;
 		stat = multi->SaveAdd(page2, &encoderParameters);
-		if (stat == Ok)
-			printf("Page 2 saved successfully.\n");
-
-		// Save the third page (frame).
-		parameterValue = EncoderValueFrameDimensionPage;
-		stat = multi->SaveAdd(page3, &encoderParameters);
-		if (stat == Ok)
-			printf("Page 3 saved successfully.\n");
-
-		// Save the fourth page (frame).
-		parameterValue = EncoderValueFrameDimensionPage;
-		stat = multi->SaveAdd(page4, &encoderParameters);
-		if (stat == Ok)
-			printf("Page 4 saved successfully.\n");
-
+		
 		// Close the multiframe file.
 		parameterValue = EncoderValueFlush;
 		stat = multi->SaveAdd(&encoderParameters);
-		if (stat == Ok)
-			printf("File closed successfully.\n");
 
 		delete multi;
 		delete page2;
-		delete page3;
-		delete page4;
 		GdiplusShutdown(gdiplusToken);
 }
