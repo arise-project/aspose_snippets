eb496e7349fca3bdb9f3d93fbad55c00d36cdd82
Evgeny.Pirogov <Evgeny.Pirogov@aspose.com>
2022-12-28 13:46:48 +0200

    bmp

--- cpp/merge/_Merger_pdf_bmp.cpp
+++ cpp/merge/_Merger_pdf_bmp.cpp
@@ -4,10 +4,19 @@
 #include "Aspose.PDF.Cpp/Generator/Image.h"
 #include "Aspose.PDF.Cpp/Devices/Device.h"
 #include "Aspose.PDF.Cpp/Page.h"
+#include "Aspose.PDF.Cpp/Generator/PageInfo.h"
 #include "Aspose.PDF.Cpp/PageCollection.h"
 #include "Aspose.PDF.Cpp/Devices/BmpDevice.h"
 #include "Aspose.PDF.Cpp/Devices/ImageDevice.h"
 #include "Aspose.PDF.Cpp/Rectangle.h"
+#include "system/io/file.h"
+#include "drawing/image.h"
+#include "drawing/bitmap.h"
+#include "drawing/graphics.h"
+
+#include <string>
+#include <Aspose.PDF.Cpp/PageSize.h>
+#include <Aspose.PDF.Cpp/SvgSaveOptions.h>
 
 using namespace System;
 using namespace Aspose::Pdf;
@@ -17,88 +26,66 @@ void pdf_to_bmp()
         String pathSource = u"../../TestData/test.pdf";
 
         // read pdf file to Aspose Document
-        System::SharedPtr<Document> doc = MakeObject<Document>(inputFilename);
+        System::SharedPtr<Document> doc = MakeObject<Document>(pathSource);
 
         // make list of path to temporary images
-        String images[doc->get_Pages()->get_Count()];
+        String * images = new String[doc->get_Pages()->get_Count()];
 
         int newWidth = 0;
         int newHeight = 0;
 
         // pages in pdf counted from 1 to n
-        for (int pageCount = 1; pageCount <= doc.getPages().size(); pageCount++)
+        int pageCount = 1;
+        for (auto const& page : doc->get_Pages())
         {
                 // setup default resolution to pdf documents 72dpi
-                com.aspose.pdf.devices.Resolution resolution = new com.aspose.pdf.devices.Resolution(72);
+                auto resolution = MakeObject<Devices::Resolution>(72);
 
                 // create image device to save document as image with page dimensions and resolution
-                com.aspose.pdf.devices.BmpDevice imageDevice = new com.aspose.pdf.devices.BmpDevice(
-                    (int)doc.getPages().get_Item(pageCount).getPageInfo().getWidth(),
-                    (int)doc.getPages().get_Item(pageCount).getPageInfo().getHeight(),
+                auto imageDevice = MakeObject<Devices::BmpDevice>(
+                    page->get_PageInfo()->get_Width(),
+                    page->get_PageInfo()->get_Height(),
                     resolution);
 
-                String outPath = "test_" + pageCount + ".bmp";
-
                 // process document page to image
-                imageDevice.process(doc.getPages().get_Item(pageCount), outPath);
-                images.add(outPath);
-        }
-
-        // make list pf parsed image sizes
-        ArrayList<com.aspose.imaging.Size> imageSizes = new ArrayList<>();
-        for (String path : images)
-        {
-                // load image from file, it supports a lot of formats
-                com.aspose.imaging.RasterImage image =
-                    (com.aspose.imaging.RasterImage)com.aspose.imaging.Image.load(path);
-                imageSizes.add(image.getSize());
+                String outPath = String::Format(u"{0}_test.bmp", pageCount);
+                auto stream = System::IO::File::Create(outPath);
+                imageDevice->Process(page, stream);
+                images[pageCount - 1] = outPath;
+                pageCount++;
         }
 
         int newWidth = 0;
         int newHeight = 0;
-        for (com.aspose.imaging.Size s : imageSizes)
+
+        for (int i =0; i < sizeof(images); i++)
         {
-                newWidth += s.getWidth();
-                newHeight = newHeight < s.getHeight() ? s.getHeight() : newHeight;
+            String path = images[i];
+            // load image from file, it supports a lot of formats
+            auto image = System::Drawing::Image::FromFile(fs);
+            // read image dimensions to pdf page rectangle
+            auto rect = new Aspose::Pdf::Rectangle(0, 0, image->get_Width() - 1, image->get_Height() - 1);
+            
+            newWidth += image->get_Width();
+            newHeight = newHeight < image->get_Height() ? image->get_Height() : newHeight;
         }
 
-        // use file system as source for save image
-        com.aspose.imaging.Source fileSource = new com.aspose.imaging.sources.FileCreateSource(
-            "./test.bmp",
-            false); // preserve image on the disk
-
-        com.aspose.imaging.imageoptions.BmpOptions options = new com.aspose.imaging.imageoptions.BmpOptions();
-        options.setSource(fileSource);
-
-        // create empty image with calculated witdh and hight
-        com.aspose.imaging.fileformats.bmp.BmpImage newImage = (com.aspose.imaging.fileformats.bmp.BmpImage)com.aspose.imaging.Image.create(options, newWidth, newHeight);
-
+        auto newImage = new System::Drawing::Bitmap(newWidth, newHeight);
+        auto canvas = System::Drawing::Graphics::FromImage(newImage);
+        canvas->set_InterpolationMode(System::Drawing::Drawing2D::InterpolationMode::HighQualityBicubic);
         int stitchedWidth = 0;
-
-        for (int i = 1; i < sizeof(images); i++)
+        for (int i = 0; i < sizeof(images); i++)
         {
                 auto fs = images[i];
 
-                // TODO: read image size
-
                 // load image from file, it supports a lot of formats
-                com.aspose.imaging.RasterImage image = (com.aspose.imaging.RasterImage)com.aspose.imaging.Image.load(imagePath);
-                // create bounds to insert small image into large
-                com.aspose.imaging.Rectangle bounds = new com.aspose.imaging.Rectangle(
-                    stitchedWidth,
-                    0,
-                    image.getWidth(),
-                    image.getHeight());
-
-                // combining images into new one
-                newImage.saveArgb32Pixels(
-                    // where to insert image
-                    bounds,
-                    // convert image chunk to 32bit Argb
-                    image.loadArgb32Pixels(image.getBounds()));
-                stitchedWidth += image.getWidth();
+                auto image = System::Drawing::Image::FromFile(fs);
+                canvas->DrawImage(image, stitchedWidth, 0);
+                stitchedWidth += image->get_Width();
         }
 
+        canvas->Save();
+
         // save created image to disk
-        newImage.save();
+        newImage->Save(u"test.bmp", System::Drawing::Imaging::ImageFormat::get_Bmp());
 }
