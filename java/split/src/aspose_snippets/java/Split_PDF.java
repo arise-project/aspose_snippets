71601c3d9936e70b0b859341943c5ccb7c0fa6be
Evgeny.Pirogov <Evgeny.Pirogov@aspose.com>
2022-12-30 00:02:58 +0200

    java split

--- java/split/src/aspose_snippets/java/Split_PDF.java
+++ java/split/src/aspose_snippets/java/Split_PDF.java
@@ -3,17 +3,17 @@ package aspose_snippets.java;
 public class Split_PDF {
     public static void Execute() {
         String pathSource = "../../TestData/test.pdf";
-        var pdfEditor = new com.aspose.pdf.PdfFileEditor();
+        var pdfEditor = new com.aspose.pdf.facades.PdfFileEditor();
         int beg = 1, end = 1;
     
         var doc = new com.aspose.pdf.Document(pathSource);
-        end = doc.Pages.Count;
+        end = doc.getPages().size();
     
         if(end > 1)
         {
             end /= 2;
         }
     
-        pdfEditor.Extract(pathSource, beg, end, "half.pdf");
+        pdfEditor.extract(pathSource, beg, end, "half.pdf");
     }
 }
