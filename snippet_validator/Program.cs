18fa7c6292c7a2e0716e71b7dc0793f36d3288d2
Evgeny.Pirogov <Evgeny.Pirogov@aspose.com>
2022-12-30 17:58:54 +0200

    callers

--- snippet_validator/Program.cs
+++ snippet_validator/Program.cs
@@ -7,7 +7,23 @@ namespace snippet_validator
     {
         static void Main(string[] args)
         {
-        	Dictionary<string, string []> runnerOpts = new Dictionary<string, string []>();
+        	Dictionary<string, string []> runnerOpts = new Dictionary<string, string []> {
+				{"merger_cpp", new string [] { "../cpp/merge/bin/debug/merge.exe" } },
+				{"split_cpp", new string [] { "../cpp/split/bin/debug/split.exe" } },
+				{"watermark_cpp", new string [] { "../cpp/watermark/bin/debug/watermark.exe" } },
+
+				{"merger_java", new string [] { "java", "-jar", "../java/merge/target/merge.jar" } },
+				{"split_java", new string [] { "java", "-jar", "../java/split/target/split.jar" } },
+				{"watermark_java", new string [] { "java", "-jar", "../java/watermark/target/watermark.jar" } },
+
+				{"merger_net", new string [] { "dotnet", "../net/merge/bin/debug/merge.dll" } },
+				{"split_net", new string [] { "dotnet", "../net/split/bin/debug/split.dll" } },
+				{"watermark_net", new string [] { "dotnet", "../net/watermark/bin/debug/watermark.dll" } },
+
+				{"merger_python", new string [] { "python3", "../python/merge/main.py" } },
+				{"split_python", new string [] { "python3", "../python/split/main.py" } },
+				{"watermark_python", new string [] { "python3", "../python/watermark/main.py" } },
+			};
         	
         	string [] outputExts = new string [] 
         	{
