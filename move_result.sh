90c7ed6891605671532af431877e77b8e9610680
Evgeny <Evgeny.Pirogov@aspose.com>
2023-01-04 20:58:54 +0200

    files

---
+++ move_result.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+mv python/merge/Merger_pdf_emf.epub TestResult/python/merge/Merger_pdf_emf.epub
+mv python/merge/Merger_pdf_pptx.pptx TestResult/python/merge/Merger_pdf_pptx.pptx
+mv python/merge/Merger_pdf_svg.svg TestResult/python/merge/Merger_pdf_svg.svg
+mv python/merge/Merger_pdf_svg_2.svg TestResult/python/merge/Merger_pdf_svg_2.svg
+mv python/merge/Merger_pdf_svg_3.svg TestResult/python/merge/Merger_pdf_svg_3.svg
+mv python/merge/Merger_pdf_svg_4.svg TestResult/python/merge/Merger_pdf_svg_4.svg
+mv python/merge/Merger_pdf_tex.tex TestResult/python/merge/Merger_pdf_tex.tex
+mv python/merge/Merger_pdf_xps.xps TestResult/python/merge/Merger_pdf_xps.xps
+mv python/merge/Merger_svg_pdf.pdf TestResult/python/merge/Merger_svg_pdf.pdf
+mv python/merge/Merger_tex_pdf.pdf TestResult/python/merge/Merger_tex_pdf.pdf
+
+
+mv python/merge/Merger_cgm_pdf.pdf TestResult/python/merge/Merger_cgm_pdf.pdf
+mv python/merge/Merger_xps_pdf.pdf TestResult/python/merge/Merger_xps_pdf.pdf
+
+mv python/merge/Merger_eps_pdf.pdf TestResult/python/merge/Merger_eps_pdf.pdf
+mv python/merge/Merger_epub_pdf.pdf TestResult/python/merge/Merger_epub_pdf.pdf
+mv python/merge/Merger_md_pdf.pdf TestResult/python/merge/Merger_md_pdf.pdf
+mv python/merge/Merger_ps_pdf.pdf TestResult/python/merge/Merger_ps_pdf.pdf
+
+mv python/merge/Merger_pcl_pdf.pdf TestResult/python/merge/Merger_pcl_pdf.pdf
+mv python/merge/Merger_pdf_doc.doc TestResult/python/merge/Merger_pdf_doc.doc
+
+mv python/merge/Merger_html_pdf.pdf TestResult/python/merge/Merger_html_pdf.pdf
+mv python/merge/Merger_pdf_docx.docx TestResult/python/merge/Merger_pdf_docx.docx
+mv python/merge/Merger_pdf_text.txt TestResult/python/merge/Merger_pdf_text.txt
+
+mv python/merge/Merger_pdf_epub.html TestResult/python/merge/Merger_pdf_epub.html
+mv python/merge/Merger_pdf_pdfa.pdf TestResult/python/merge/Merger_pdf_pdfa.pdf
+mv python/merge/Merger_pdf_xls.xls TestResult/python/merge/Merger_pdf_xls.xls
+
+
+mv python/merge/Merger_bmp_pdf.pdf TestResult/python/merge/Merger_bmp_pdf.pdf
+mv python/merge/Merger_jpg_docx.docx TestResult/python/merge/Merger_jpg_docx.docx
+mv python/merge/Merger_jpg_pdf.pdf TestResult/python/merge/Merger_jpg_pdf.pdf
+
+mv python/merge/Merger_pdf_bmp.bmp TestResult/python/merge/Merger_pdf_bmp.bmp
+
+mv python/merge/Merger_pdf_html.pdf TestResult/python/merge/Merger_pdf_html.pdf
+mv python/merge/Merger_pdf_jpeg.jpg TestResult/python/merge/Merger_pdf_jpeg.jpg
+mv python/merge/Merger_pdf_png.png TestResult/python/merge/Merger_pdf_png.png
+mv python/merge/Merger_pdf_tiff.tiff TestResult/python/merge/Merger_pdf_tiff.tiff
+mv python/merge/Merger_png_pdf.pdf TestResult/python/merge/Merger_png_pdf.pdf
+
+
+mv python/merge/Merger_tiff_pdf.pdf TestResult/python/merge/Merger_tiff_pdf.pdf
+
+ 
